90ff7c4dbfec9e7ddc9c288cc4920d4bd5d0c77b
Hari Babu <hari.babu@perficient.com>
2025-08-07 20:33:18 +0530

    application development completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'typing-game';
+  enteredText = '';
+  sentence = faker.lorem.sentence();
+  compare(generatedLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return generatedLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
